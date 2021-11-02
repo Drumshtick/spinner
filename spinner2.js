60d7c2f4167eff28a3a3c5ff85d69301c6ac88d2
Drumshtick <ekodnar@outlook.com>
2021-11-02 18:40:57 +0000

    Removed test code that wasn't deleted

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,8 @@
 const frames = ['|', '/', '-', '\\'];
 let time = 100;
-let i = 0;
 for (const frame of frames) {
   setTimeout(() => {
     process.stdout.write(`\r${frame}   `);
   }, time);
   time += 200;
-  i++;
 }
